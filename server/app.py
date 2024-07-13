468eb1f54f77706e46b83767f6999b227b0849d6
emmanuel <emmanuel.kisoso@student.moringaschool.com>
2024-07-13 15:28:26 +0300

    Final Commit

--- server/app.py
+++ server/app.py
@@ -14,13 +14,44 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET'])
 def messages():
-    return ''
+    messages = Message.query.all()
+    return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods = ['POST'])
+def create_messages():
+    data = request.get_json()
+    new_message = Message(
+        body = data.get('body'),
+        username= data.get('username')
+        )
+
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods = ['GET'])
+def message_by_id(id):
+    message = Message.query.get_or_404(id)
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods = ['PUT', 'PATCH'])
+def update_message(id):
+    data = request.get_json()
+    message = Message.query.get_or_404(id)
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods = ['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, func
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -12,3 +12,16 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable = False)
+    username = db.Column(db.String, nullable = False)
+    created_at = db.Column(db.DateTime, server_default = func.now())
+    updated_at = db.Column(db.DateTime, onupdate = func.now(), server_default = func.now())
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
